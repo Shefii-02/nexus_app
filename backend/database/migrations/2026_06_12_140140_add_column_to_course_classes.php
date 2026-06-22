c62550ff39803c547ca9bce02749a02588cbc369
Shefii-02 <shefii.km@gmail.com>
2026-06-22 19:49:21 +0530

    course_classes

---
+++ backend/database/migrations/2026_06_12_140140_add_column_to_course_classes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+          Schema::table('course_classes', function (Blueprint $table) {
+            $table->dropForeign(['teacher_id']);
+            $table->dropColumn('teacher_id');
+            $table->foreignId('teacher_id')->constrained('users')->cascadeOnDelete()->after('course_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('course_classes', function (Blueprint $table) {
+            //
+        });
+    }
+};

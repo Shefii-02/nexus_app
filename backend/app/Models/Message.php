680f9a15e02d7c07338710032c9a47933f0fb095
Shefii-02 <shefii.km@gmail.com>
2026-07-15 22:26:08 +0530

    fix

--- backend/app/Models/Message.php
+++ backend/app/Models/Message.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Message extends Model
 {
@@ -20,4 +23,57 @@ class Message extends Model
     {
         return $this->hasOne(Poll::class);
     }
+
+        public function sender(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\User::class, 'sender_id');
+    }
+
+    public function replyTo(): BelongsTo
+    {
+        return $this->belongsTo(\App\Chat\Models\Message::class, 'reply_to');
+    }
+
+
+
+    public function media(): BelongsTo
+    {
+        return $this->belongsTo(
+            MediaFile::class,
+            'media_url'
+        );
+    }
+
+    public function reads(): HasMany
+    {
+        return $this->hasMany(\App\Models\MessageRead::class);
+    }
+
+
+
+    public function reports(): HasMany
+    {
+        return $this->hasMany(\App\Models\MessageReport::class);
+    }
+
+    public function deletedFor(): HasMany
+    {
+        return $this->hasMany(\App\Models\DeletedMessage::class);
+    }
+
+    // public function scopeVisibleTo($query, int $userId)
+    // {
+    //     return $query->whereDoesntHave('deletedFor', fn($q) => $q->where('user_id', $userId))
+    //                  ->where('is_deleted', false);
+    // }
+
+    public function scopeVisibleTo($query, int $userId)
+    {
+        return $query->whereDoesntHave(
+            'deletedFor',
+            fn($q) => $q->where('user_id', $userId)
+        );
+    }
+
+
 }

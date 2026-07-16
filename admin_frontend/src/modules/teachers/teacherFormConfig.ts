77e6ed154085b10659df1a45be67e1964fcec7b0
Shefii-02 <shefii.km@gmail.com>
2026-07-17 00:34:22 +0530

    teacher create

--- admin_frontend/src/modules/teachers/teacherFormConfig.ts
+++ admin_frontend/src/modules/teachers/teacherFormConfig.ts
@@ -2,7 +2,7 @@ export const teacherFormConfig = [
   { name: 'name', label: 'Name', type: 'text', required: true, section: 'user_details' },
   { name: 'email', label: 'Email', type: 'email', required: true, section: 'user_details' },
   { name: 'phone', label: 'Phone', type: 'text', required: true, section: 'user_details' },
-  { name: 'password', label: 'Password', type: 'password', section: 'user_details', hideOnEdit: true },
+  // { name: 'password', label: 'Password', type: 'password', section: 'user_details', hideOnEdit: true },
 
   // { name: 'subject', label: 'Subject', type: 'text', section: 'teacher_details' },
   // { name: 'qualification', label: 'Qualification', type: 'text', section: 'teacher_details' },

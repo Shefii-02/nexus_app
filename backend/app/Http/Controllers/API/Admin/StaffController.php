8dda17f06dfcf1334e2157d14fa03c0412a6c13f
Shefii-02 <shefii.km@gmail.com>
2026-07-20 03:33:58 +0530

    fix

--- backend/app/Http/Controllers/API/Admin/StaffController.php
+++ backend/app/Http/Controllers/API/Admin/StaffController.php
@@ -193,7 +193,7 @@ class StaffController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'Permissions updated successfully',
-            'data' => $user->permissions_map,
+            'data' => $user->permissions,
         ]);
     }
 }

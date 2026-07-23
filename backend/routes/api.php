d4c3e426169599caead807d58924c03bb346b873
Shefii-02 <shefii.km@gmail.com>
2026-07-23 11:49:58 +0530

    fix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -161,7 +161,7 @@ Route::middleware(['auth:api'])->group(function () {
 
     // Staff Management
 
-    Route::put('staff/{id}/permission-update', [StaffController::class, 'permissionUpdate']);
+    Route::put('staffs/{id}/permission-update', [StaffController::class, 'permissionUpdate']);
     Route::apiResource('staff', StaffController::class);
 
     Route::apiResource('roles', RoleController::class);

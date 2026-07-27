4220c0de757481aa018d124656fc98f9e074bfbd
Shefii-02 <shefii.km@gmail.com>
2026-07-27 23:03:12 +0530

    fix

--- backend/app/Http/Controllers/API/Admin/StudentController.php
+++ backend/app/Http/Controllers/API/Admin/StudentController.php
@@ -171,6 +171,7 @@ class StudentController extends Controller
             $this->deleteUserData($studentUser);
 
             // $user->forceDelete(); // ← permanent delete, bypasses SoftDeletes
+            User::findOrFail($staff)->forceDelete();
 
 
             return $this->successResponse(null, 'Student deleted successfully');
